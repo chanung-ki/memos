7d62cfdf5c48a3a7d44d3e3f709d9ecdd79d3d95
chanung-ki <cksdnd7416@gmail.com>
2023-02-16 15:24:35 +0900

    create models
    delete member app
    create models (Users, Onememos)

--- onememos/admin.py
+++ onememos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from onememos.models import Users, OneMemo
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(OneMemo)
--- onememos/models.py
+++ onememos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class OneMemo(models.Model):
+    content = models.CharField(max_length=100)
+    writer = models.CharField(max_length=50)
+    update_Date = models.DateTimeField(auto_now=True)
+    write_date = models.DateTimeField(auto_now_add=True)
+    
+
+class Users(AbstractUser):
+    testcol = models.CharField(max_length=50, null=True)
